4a8d7cd9b6c43e5135d98b3feb98d54ae85dc0e7
Kylie <williamkyle484@gmail.com>
2025-11-22 05:01:07 -0300

    atualizacao do core/forms

--- core/forms.py
+++ core/forms.py
@@ -6,18 +6,17 @@ from django.db import transaction
 from .models import Profile, Curso, DisponibilidadeSala
 
 class CursoForm(forms.ModelForm):
-    # Campo extra para selecionar a agenda
     agenda_disponivel = forms.ModelChoiceField(
         queryset=DisponibilidadeSala.objects.filter(livre=True),
         label="Escolha uma Sala e Horário Disponível",
         widget=forms.Select(attrs={'class': 'form-select'}),
-        empty_label="Selecione uma opção..."
+        empty_label="Selecione uma opção...",
+        required=False # Opcional, caso o prof queira criar sem agenda por enquanto
     )
 
     class Meta:
         model = Curso
         fields = ['nome_curso', 'descricao'] 
-        # Removemos 'agenda' do fields direto porque vamos tratar manualmente
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,32 +25,56 @@ class CursoForm(forms.ModelForm):
 
     def save(self, commit=True):
         curso = super().save(commit=False)
-        # Pega a agenda selecionada
-        agenda = self.cleaned_data['agenda_disponivel']
         
-        # Marca a agenda como ocupada (livre=False)
-        agenda.livre = False
-        agenda.save()
-        
-        # Associa ao curso
-        curso.agenda = agenda
+        # Se uma agenda foi selecionada, usa ela
+        if self.cleaned_data.get('agenda_disponivel'):
+            agenda = self.cleaned_data['agenda_disponivel']
+            agenda.livre = False
+            agenda.save()
+            curso.agenda = agenda
         
         if commit:
             curso.save()
         return curso
 
-# ... (O CustomUserCreationForm continua igual, não precisa mudar)
+
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, required=True, label="Nome", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100, required=False, label="Apelido", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    ROLE_CHOICES = [('aluno', 'Aluno'), ('professor', 'Professor')]
-    role = forms.ChoiceField(choices=ROLE_CHOICES, required=True, label="Eu sou:", widget=forms.Select(attrs={'class': 'form-select'}))
+    first_name = forms.CharField(
+        max_length=100, 
+        required=True, 
+        label="Nome",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        max_length=100, 
+        required=False, 
+        label="Apelido",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        required=True, 
+        label="Email",
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    
+    ROLE_CHOICES = [
+        ('aluno', 'Aluno'),
+        ('professor', 'Professor'),
+    ]
+    role = forms.ChoiceField(
+        choices=ROLE_CHOICES,
+        required=True,
+        label="Eu sou:",
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-        widgets = {'username': forms.TextInput(attrs={'class': 'form-control'})}
+        
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -60,13 +83,22 @@ class CustomUserCreationForm(UserCreationForm):
         self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
         self.fields['password2'].label = "Confirme a Senha"
 
+
     @transaction.atomic
     def save(self, commit=True):
-        user = super().save(commit=False)
+        # 1. Salva o User
+        # (Isso dispara o sinal no models.py que CRIA o Profile vazio)
+        user = super().save(commit=True) 
+        
+        # 2. Atualiza os dados extras do User
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-            Profile.objects.create(user=user, role=self.cleaned_data['role'])
+        user.save()
+
+        # 3. ATUALIZA o Perfil (em vez de criar um novo)
+        # O perfil já existe graças ao sinal, só precisamos preencher o role
+        user.profile.role = self.cleaned_data['role']
+        user.profile.save()
+
         return user
